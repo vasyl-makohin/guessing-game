489c0419983a57ed27681a4eadfc0bd1a746f399
vasyl-makohin <makohin.v@gmail.com>
2022-08-13 00:50:55 +0300

    feat: add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,27 @@
 class GuessingGame {
-    constructor() {}
+  constructor() {
+    this.guessedNumber = 0;
+    this.min = 0;
+    this.max = 0;
+  }
 
-    setRange(min, max) {
+  setRange(min, max) {
+    this.min = min;
+    this.max = max;
+  }
 
-    }
+  guess() {
+    this.guessedNumber = Math.round((this.max + this.min) / 2);
+    return this.guessedNumber;
+  }
 
-    guess() {
+  lower() {
+    this.max = this.guessedNumber;
+  }
 
-    }
-
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+  greater() {
+    this.min = this.guessedNumber;
+  }
 }
 
 module.exports = GuessingGame;
